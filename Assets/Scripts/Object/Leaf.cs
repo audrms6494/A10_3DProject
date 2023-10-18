e4fdbe3e6838c332d611ea4be36071725f891330
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-10-18 21:34:59 +0900

    Object script의 확장성 증가 및 맵 이스터에그 추가

--- Assets/Scripts/Object/Leaf.cs
+++ Assets/Scripts/Object/Leaf.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Leaf : MonoBehaviour
 {
     public bool gameStart;
+    public Vector3 destinationPoisition;
     public void Update()
     {
         if (gameStart)
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 0, 50f), 0.01f);
+            transform.position = Vector3.MoveTowards(transform.position, destinationPoisition, 0.01f);
         }
     }
 }
