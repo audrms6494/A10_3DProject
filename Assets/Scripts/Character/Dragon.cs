42eba60c65fa73d50eab89f221234454e3136a32
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-10-20 01:47:24 +0900

    DragonScript 생성

---
+++ Assets/Scripts/Character/Dragon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Dragon : MonoBehaviour
+{
+    public string dragonName;
+    [SerializeField] private PlayerInput playerInput;
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // var ui = UIManager.ShowUI<UIGameClear>();
+            // ui.Initalize(playerInput, dragonName);
+        }
+    }
+}
