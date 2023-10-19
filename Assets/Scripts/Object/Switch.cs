b181cebcaf3c24f06511fab24582a8f144280bdc
NewRound <86642886+NewRound@users.noreply.github.com>
2023-10-19 11:00:28 +0900

    상호작용 (Interactable) 실험 완료

--- Assets/Scripts/Object/Switch.cs
+++ Assets/Scripts/Object/Switch.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : OnOffObject
-{ 
+public class Switch : OnOffObject, IInteractable
+{
+    [SerializeField] public string Name;
 
 
+    public string GetInteractPrompt()
+    {
+        return Name;
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("Active");
+        Use();
+    }
 }
