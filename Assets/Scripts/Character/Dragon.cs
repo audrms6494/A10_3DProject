4a1d5575e5b0fd7b7ea329a62b234ec299848c67
YJS <90309463+noyyo@users.noreply.github.com>
2023-10-20 11:18:54 +0900

    GameManager수정 (#15)

--- Assets/Scripts/Character/Dragon.cs
+++ Assets/Scripts/Character/Dragon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Dragon : MonoBehaviour
 {
@@ -15,6 +16,19 @@ public class Dragon : MonoBehaviour
             Cursor.lockState = CursorLockMode.Confined;
             var ui = UIManager.ShowUI<UIGameClear>();
             ui.Initalize(playerInput, dragonName);
+            string sceneName = SceneManager.GetActiveScene().name;
+            switch (sceneName)
+            {
+                case "PlayerTestScene":
+                    GameManager.Instance.StageClearFlags[0] = true;
+                    break;
+                case "WaterScene":
+                    GameManager.Instance.StageClearFlags[1] = true;
+                    break;
+                case "ObjectTest":
+                    GameManager.Instance.StageClearFlags[2] = true;
+                    break;
+            }
         }
     }
 }
